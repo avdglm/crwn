e2551cf113f0f78dc98a1ecd8c681a856300992d
avdglm <arvidgilljam@gmail.com>
2022-09-04 21:25:29 +0200

    added cart icon and cart items in cart dropdown

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,15 +1,19 @@
-import { Fragment, useContext } from 'react';
-import { Outlet, Link } from 'react-router-dom';
+import { Fragment, useContext } from "react";
+import { Outlet, Link } from "react-router-dom";
 
-import { UserContext } from '../../contexts/user.context';
+import { UserContext } from "../../contexts/user.context";
+import { CartContext } from "../../contexts/cart.context";
 
-import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
-import { signOutUser } from '../../utils/firebase/firebase.utils';
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
+import { signOutUser } from "../../utils/firebase/firebase.utils";
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 
-import './navigation.styles.scss';
+import "./navigation.styles.scss";
 
 const Navigation = () => {
   const { currentUser, setCurrentUser } = useContext(UserContext);
+  const { isCartOpen } = useContext(CartContext)
 
   const signOutHandler = async () => {
     await signOutUser();
@@ -18,25 +22,28 @@ const Navigation = () => {
 
   return (
     <Fragment>
-      <div className='navigation'>
-        <Link className='logo-container' to='/'>
-          <CrwnLogo className='logo' />
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <CrwnLogo className="logo" />
         </Link>
-        <div className='nav-links-container'>
-          <Link className='nav-link' to='/shop'>
+        <div className="nav-links-container">
+          
+          <Link className="nav-link" to="/shop">
             SHOP
           </Link>
 
           {currentUser ? (
-            <span className='nav-link' onClick={signOutHandler}>
-              {' '}
-              SIGN OUT{' '}
+            <span className="nav-link" onClick={signOutHandler}>
+              {" "}
+              SIGN OUT{" "}
             </span>
           ) : (
-            <Link className='nav-link' to='/auth'>
+            <Link className="nav-link" to="/auth">
               SIGN IN
             </Link>
           )}
+          <CartIcon />
+          {isCartOpen && <CartDropdown />}
         </div>
       </div>
       <Outlet />
